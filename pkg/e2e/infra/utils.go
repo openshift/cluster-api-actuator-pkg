311ba82a9219223a195165e6b19ea947ec864fc0
Enxebre <alberto.garcial@hotmail.com>
2019-03-06 16:13:07 +0100

    Add scaling stories

--- pkg/e2e/infra/utils.go
+++ pkg/e2e/infra/utils.go
@@ -10,14 +10,22 @@ import (
 	mapiv1beta1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
 	controllernode "github.com/openshift/cluster-api/pkg/controller/node"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/scale"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/utils/pointer"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 const (
 	nodeWorkerRoleLabel = "node-role.kubernetes.io/worker"
+	machineRoleLabel    = "machine.openshift.io/cluster-api-machine-role"
+	machineAPIGroup     = "machine.openshift.io"
 )
 
 func isOneMachinePerNode(client runtimeclient.Client) bool {
@@ -118,6 +126,52 @@ func getMachineSetList(client runtimeclient.Client) (*mapiv1beta1.MachineSetList
 	return &machineSetList, nil
 }
 
+// getMachineSets returns the list of machineSets or an error
+func getMachineSets(client runtimeclient.Client) ([]mapiv1beta1.MachineSet, error) {
+	machineSetList, err := getMachineSetList(client)
+	if err != nil {
+		return nil, fmt.Errorf("error getting machineSetList: %v", err)
+	}
+	if len(machineSetList.Items) == 0 {
+		return nil, fmt.Errorf("error getting machineSets, machineSetList is empty")
+	}
+	return machineSetList.Items, nil
+}
+
+// getMachineList returns a MachineList object
+func getMachineList(client runtimeclient.Client) (*mapiv1beta1.MachineList, error) {
+	machineList := mapiv1beta1.MachineList{}
+	listOptions := runtimeclient.ListOptions{
+		Namespace: e2e.TestContext.MachineApiNamespace,
+	}
+	if err := wait.PollImmediate(1*time.Second, time.Minute, func() (bool, error) {
+		if err := client.List(context.TODO(), &listOptions, &machineList); err != nil {
+			glog.Errorf("error querying api for machineList object: %v, retrying...", err)
+			return false, nil
+		}
+		return true, nil
+	}); err != nil {
+		glog.Errorf("Error calling getMachineList: %v", err)
+		return nil, err
+	}
+	return &machineList, nil
+}
+
+// getMachinesFromMachineSet returns an array of machines owned by a given machineSet
+func getMachinesFromMachineSet(client runtimeclient.Client, machineSet mapiv1beta1.MachineSet) ([]mapiv1beta1.Machine, error) {
+	machineList, err := getMachineList(client)
+	if err != nil {
+		return nil, fmt.Errorf("error getting machineList: %v", err)
+	}
+	var machinesForSet []mapiv1beta1.Machine
+	for key := range machineList.Items {
+		if metav1.IsControlledBy(&machineList.Items[key], &machineSet) {
+			machinesForSet = append(machinesForSet, machineList.Items[key])
+		}
+	}
+	return machinesForSet, nil
+}
+
 // getMachineFromNode returns the machine referenced by the "controllernode.MachineAnnotationKey" annotation in the given node
 func getMachineFromNode(client runtimeclient.Client, node *corev1.Node) (*mapiv1beta1.Machine, error) {
 	machineNamespaceKey, ok := node.Annotations[controllernode.MachineAnnotationKey]
@@ -155,6 +209,41 @@ func deleteMachine(client runtimeclient.Client, machine *mapiv1beta1.Machine) er
 	})
 }
 
+// getNodesFromMachineSet returns an array of nodes backed by machines owned by a given machineSet
+func getNodesFromMachineSet(client runtimeclient.Client, machineSet mapiv1beta1.MachineSet) ([]*corev1.Node, error) {
+	machines, err := getMachinesFromMachineSet(client, machineSet)
+	if err != nil {
+		return nil, fmt.Errorf("error calling getMachinesFromMachineSet %v", err)
+	}
+
+	var nodes []*corev1.Node
+	for key := range machines {
+		node, err := getNodeFromMachine(client, machines[key])
+		if err != nil {
+			return nil, fmt.Errorf("error getting node from machine %q: %v", machines[key].Name, err)
+		}
+		nodes = append(nodes, node)
+	}
+	glog.Infof("MachineSet %q have %d nodes", machineSet.Name, len(nodes))
+	return nodes, nil
+}
+
+// getNodeFromMachine returns the node object referenced by machine.Status.NodeRef
+func getNodeFromMachine(client runtimeclient.Client, machine mapiv1beta1.Machine) (*corev1.Node, error) {
+	var node corev1.Node
+	if machine.Status.NodeRef == nil {
+		glog.Errorf("Machine %q has no NodeRef", machine.Name)
+		return nil, fmt.Errorf("machine %q has no NodeRef", machine.Name)
+	}
+	key := runtimeclient.ObjectKey{Namespace: machine.Status.NodeRef.Namespace, Name: machine.Status.NodeRef.Name}
+	if err := client.Get(context.Background(), key, &node); err != nil {
+		return nil, fmt.Errorf("error getting node %q: %v", node.Name, err)
+	}
+
+	glog.Infof("Machine %q is backing node %q", machine.Name, node.Name)
+	return &node, nil
+}
+
 // getWorkerNode returns a node with the nodeWorkerRoleLabel label
 func getWorkerNode(client runtimeclient.Client) (*corev1.Node, error) {
 	nodeList := corev1.NodeList{}
@@ -175,4 +264,58 @@ func getWorkerNode(client runtimeclient.Client) (*corev1.Node, error) {
 		return nil, err
 	}
 	return &nodeList.Items[0], nil
+}
+
+// nodesAreReady returns true if an array of nodes are all ready
+func nodesAreReady(nodes []*corev1.Node) bool {
+	// All nodes needs to be ready
+	for key := range nodes {
+		if !e2e.IsNodeReady(nodes[key]) {
+			glog.Errorf("Node %q is not ready. Conditions are: %v", nodes[key].Name, nodes[key].Status.Conditions)
+			return false
+		}
+		glog.Infof("Node %q is ready. Conditions are: %v", nodes[key].Name, nodes[key].Status.Conditions)
+	}
+	return true
+}
+
+// scaleMachineSet scales a machineSet with a given name to the given number of replicas
+func scaleMachineSet(name string, replicas int) error {
+	scaleClient, err := getScaleClient()
+	if err != nil {
+		return fmt.Errorf("error calling getScaleClient %v", err)
+	}
+
+	scale, err := scaleClient.Scales(e2e.TestContext.MachineApiNamespace).Get(schema.GroupResource{Group: machineAPIGroup, Resource: "MachineSet"}, name)
+	if err != nil {
+		return fmt.Errorf("error calling scaleClient.Scales get: %v", err)
+	}
+
+	scaleUpdate := scale.DeepCopy()
+	scaleUpdate.Spec.Replicas = int32(replicas)
+	_, err = scaleClient.Scales(e2e.TestContext.MachineApiNamespace).Update(schema.GroupResource{Group: machineAPIGroup, Resource: "MachineSet"}, scaleUpdate)
+	if err != nil {
+		return fmt.Errorf("error calling scaleClient.Scales update: %v", err)
+	}
+	return nil
+}
+
+// getScaleClient returns a ScalesGetter object to manipulate scale subresources
+func getScaleClient() (scale.ScalesGetter, error) {
+	cfg, err := e2e.LoadConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error getting config %v", err)
+	}
+	mapper, err := apiutil.NewDiscoveryRESTMapper(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error calling NewDiscoveryRESTMapper %v", err)
+	}
+
+	discovery := discovery.NewDiscoveryClientForConfigOrDie(cfg)
+	scaleKindResolver := scale.NewDiscoveryScaleKindResolver(discovery)
+	scaleClient, err := scale.NewForConfig(cfg, mapper, dynamic.LegacyAPIPathResolverFunc, scaleKindResolver)
+	if err != nil {
+		return nil, fmt.Errorf("error calling building scale client %v", err)
+	}
+	return scaleClient, nil
 }
