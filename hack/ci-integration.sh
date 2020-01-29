1286c29a45e20c3d2c751ea626a81cc89fa87f68
Enxebre <alberto.garcial@hotmail.com>
2020-01-29 16:47:48 +0100

    Run tests in parallel and use ginkgo cli

--- hack/ci-integration.sh
+++ hack/ci-integration.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-go test -timeout 90m \
-  -v ./pkg/e2e \
-  -kubeconfig ${KUBECONFIG:-~/.kube/config} \
-  -args -v 5 -logtostderr \
-  "$@"
+go run ./vendor/github.com/onsi/ginkgo/ginkgo \
+    -timeout 90m \
+    -p -stream \
+    -v \
+    -failFast \
+    "$@" \
+    ./pkg/e2e/ -- --alsologtostderr -v 4 -kubeconfig ${KUBECONFIG:-~/.kube/config}
