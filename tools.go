1052aa1faf4fb899d0ec648986759400d358646d
Enxebre <alberto.garcial@hotmail.com>
2020-01-29 16:46:30 +0100

    Add file for go mod to bring ginkgo cli

---
+++ tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+// Official workaround to track tool dependencies with go modules:
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+package tools
+
+import (
+	_ "github.com/onsi/ginkgo/ginkgo"
+)
