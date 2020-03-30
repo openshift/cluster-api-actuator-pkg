c708273a19b049182218e0aad4bad27d11fd079d
Joel Speed <joel.speed@hotmail.co.uk>
2020-03-30 14:09:45 +0100

    Allow E2E suite to output Junit files

--- pkg/e2e_test.go
+++ pkg/e2e_test.go
@@ -1,10 +1,15 @@
 package e2e
 
 import (
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/golang/glog"
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 
 	osconfigv1 "github.com/openshift/api/config/v1"
@@ -18,6 +23,8 @@ import (
 	_ "github.com/openshift/cluster-api-actuator-pkg/pkg/machinehealthcheck"
 	_ "github.com/openshift/cluster-api-actuator-pkg/pkg/operators"
 )
+
+const junitDirEnvVar = "JUNIT_DIR"
 
 func init() {
 	if err := mapiv1beta1.AddToScheme(scheme.Scheme); err != nil {
@@ -39,5 +46,17 @@ func init() {
 
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Machine Suite")
+	RunSpecsWithDefaultAndCustomReporters(t, "Machine Suite", e2eReporters())
+}
+
+func e2eReporters() []Reporter {
+	reportDir := os.Getenv(junitDirEnvVar)
+	if reportDir != "" {
+		// Include `ParallelNode` so tests running in parallel do not overwrite the same file.
+		// Include timestamp so test suite can be called multiple times with focus within same CI job
+		// without overwriting files.
+		junitFileName := fmt.Sprintf("%s/junit_cluster_api_actuator_pkg_e2e_%d_%d.xml", reportDir, time.Now().UnixNano(), config.GinkgoConfig.ParallelNode)
+		return []Reporter{reporters.NewJUnitReporter(junitFileName)}
+	}
+	return []Reporter{}
 }
