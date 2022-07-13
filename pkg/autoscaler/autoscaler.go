12d1d238777b0d54cdb233c4bc3979a025f12a3c
Yanhua Li <yanhua.li@nutanix.com>
2022-07-13 12:23:29 -0400

    Bug 2106403: Add Nutanix as the supported provider platform for e2e-nutanix-operator test

--- pkg/autoscaler/autoscaler.go
+++ pkg/autoscaler/autoscaler.go
@@ -254,7 +254,7 @@ var _ = Describe("[Feature:Machines] Autoscaler should", func() {
 
 			platform := clusterInfra.Status.PlatformStatus.Type
 			switch platform {
-			case configv1.AWSPlatformType, configv1.GCPPlatformType, configv1.AzurePlatformType, configv1.OpenStackPlatformType, configv1.VSpherePlatformType:
+			case configv1.AWSPlatformType, configv1.GCPPlatformType, configv1.AzurePlatformType, configv1.OpenStackPlatformType, configv1.VSpherePlatformType, configv1.NutanixPlatformType:
 				klog.Infof("Platform is %v", platform)
 			default:
 				Skip(fmt.Sprintf("Platform %v does not support autoscaling from/to zero, skipping.", platform))
